d49f0eea800e6e5fd2e7dc366a0a3ee79aa1b111
Eddy <eddy.li@pentium.network>
2018-07-13 18:33:45 +0800

    jwt decode

--- routes/gateone_log.js
+++ routes/gateone_log.js
@@ -5,8 +5,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/*', function(req, res, next) {
-  console.log(req.query);
-  res.json(req.query);
+  var token = req.query.token;
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace('-', '+').replace('_', '/');
+
+
+  // return JSON.parse(window.atob(base64));
+  res.json(JSON.parse(Buffer.from(base64, 'base64')));
 });
 
 router.post('/', function(req, res, next) {
